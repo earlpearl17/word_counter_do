6aec23bca65615de6d8b07e4b05df6770ecbc433
Earl Pearl <earlpearl17@gmail.com>
2020-03-22 07:53:44 -0400

    Enabled admin URL

--- word_counter/urls.py
+++ word_counter/urls.py
@@ -18,7 +18,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    #path('admin/', admin.site.urls),
+    path('admin/', admin.site.urls),
     path('', views.homepage, name='home'),
     path('count/', views.count, name='count'),
     path('about/', views.about, name='about')
